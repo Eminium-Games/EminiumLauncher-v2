15c501e5686ae4e3143d738e0476e5e843d6b7a3
Fourty3000 <loic.pierre.sannier@gmail.com>
2026-03-29 00:00:18 +0100

    commit

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,8 +4,7 @@ const ConfigManager = require("./configmanager");
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL =
-  "https://api.npoint.io/a4465884cc6fa2e1b0a0";
+exports.REMOTE_DISTRO_URL = "https://cdn.eminium.ovh/nebula_distribution/root/distribution.json";
 
 const api = new DistributionAPI(
   ConfigManager.getLauncherDirectory(),
